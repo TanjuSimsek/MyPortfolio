87614e09aef87a421e439e27e26516bfbaee4d71
TanjuSimsek <tanju.simsek@yandex.com>
2019-02-20 10:00:53 +0300

    MyPortFolioNew

--- MyPortFolio/MyPortFolio.Data/ApplicationDbContext.cs
+++ MyPortFolio/MyPortFolio.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using MyPortFolio.Data.Builder;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MyPortFolio.Data.Builder;
 using MyPortFolio.Model;
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,16 @@ using System.Threading.Tasks;
 
 namespace MyPortFolio.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext() : base("name=DefaultConnection") {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
 
-
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
         }
         public virtual DbSet<Post> Posts { get; set; }
         public virtual DbSet<Category> Categories { get; set; }
